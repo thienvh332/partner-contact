1116144c5ac332981f14e19f2f156c160db17851
thien <thienvh@trobz.com>
2024-08-26 14:39:33 +0700

    [ADD] partner_classification

---
+++ partner_classification/models/partner_classification.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Trobz
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, fields, models
+
+
+class PartnerClassification(models.Model):
+    _name = "partner.classification"
+
+    name = fields.Char(required=True)
+    parent_id = fields.Many2one("partner.classification", "Parent")
+
+    @api.constrains("parent_id")
+    def check_parent_id(self):
+        if not self._check_recursion():
+            raise ValueError(_("Error! You cannot create recursive classifications."))
